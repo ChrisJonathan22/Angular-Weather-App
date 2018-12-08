eec6e18bf6edcc1afdcd06190f7ba83635f64392
Chris Jonathan <christophereko@hotmail.fr>
2018-12-08 19:57:38 +0000

    Working on implementing a graph.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import * as $ from 'jquery';
 // import DataTable from 'dataTables';
 import 'DataTables.net';
 // import 'datatables';
+import 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -32,7 +33,13 @@ export class HomeComponent implements OnInit {
     let loading = document.getElementById('loading-img');
     setTimeout(() => { 
       loading.style.display = 'none';
-      $('#data-table').DataTable(); 
+      $('#data-table').DataTable({
+        "lengthMenu": [ [5, 10, 20, -1], [5, 10, 20, "All"] ],
+        "columnDefs": [
+          {"className": "dt-center", "targets": "_all"},
+          { "width": "20%", "targets": "_all" }
+        ]
+      }); 
       dataTable.style.display = '';
     }, 2000);
   
@@ -59,7 +66,7 @@ export class HomeComponent implements OnInit {
         trCountry.appendChild(trCountryText);
 
         let trTemp = document.createElement('td');
-        let trTempText = document.createTextNode(`${data.main.temp}℃`);
+        let trTempText = document.createTextNode(`${data.main.temp}`);
         trTemp.appendChild(trTempText);
 
         tr.appendChild(trName);
@@ -107,5 +114,47 @@ export class HomeComponent implements OnInit {
 
       }
     */ 
+
+    
+
+   var ctx = document.getElementById("myChart").getContext('2d');
+   var myChart = new Chart(ctx, {
+       type: 'bar',
+       data: {
+           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+           datasets: [{
+               label: 'Temperature',
+               data: [12, 19, 3, 5, 2, 3],
+               backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   'rgba(75, 192, 192, 0.2)',
+                   'rgba(153, 102, 255, 0.2)',
+                   'rgba(255, 159, 64, 0.2)'
+               ],
+               borderColor: [
+                   'rgba(255,99,132,1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 1
+           }]
+       },
+       options: {
+           scales: {
+               yAxes: [{
+                   ticks: {
+                       beginAtZero:true
+                   }
+               }]
+           }
+       }
+   });
+      
+
     }
   }
